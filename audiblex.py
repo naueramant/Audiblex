c2f2b7dec4eade861979152c3cc1a7db836ae144
Jonas Tranberg <jonas_ts@msn.com>
2019-01-02 23:14:30 +0100

    added if __main__ check

--- audiblex.py
+++ audiblex.py
@@ -4,27 +4,18 @@ import argparse, subprocess, os, re, csv
 from enum import Enum
 
 class Colors:
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+    BLUE   = '\033[94m'
+    GREEN  = '\033[92m'
+    YELLOW = '\033[93m'
+    FAIL   = '\033[91m'
+    ENDC   = '\033[0m'
 
 class Filetypes(Enum):
-    M4B='M4B'
-    M4A='M4A'
-    MP3='MP3'
+    M4B = 'M4B'
+    M4A = 'M4A'
+    MP3 = 'MP3'
 
 cach_path = '/tmp/audiblex_cache'
-
-parser = argparse.ArgumentParser(description='Convert audible AAX files to M4B, M4A or MP4')
-parser.add_argument('file', help='The aax file to convert')
-parser.add_argument('-t', '--type', help='The destination filetype M4B, M4A or MP3', type=Filetypes)
-parser.add_argument('-a', '--activation', help='Define the activation bits to use')
-parser.add_argument('-s', '--single', help='Convert to a single file', action='store_true')
-parser.add_argument('-l', '--lookup', help='Lookup the activation bits in the rainbow table', action='store_true')
-parser.add_argument('-c', '--clear', help='Clear the activation bits cache', action='store_true')
-
-args = parser.parse_args()
 
 sp = os.path.dirname(os.path.realpath(__file__))
 
@@ -78,25 +69,36 @@ def saveCache(ab: dict):
     except:
         print(Colors.FAIL + '::' + Colors.ENDC, 'Faild to save activation bits cache')
 
-try:
-    if args.clear:
-        saveCache({})
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert audible AAX files to M4B, M4A or MP4')
+    parser.add_argument('file', help='The aax file to convert')
+    parser.add_argument('-t', '--type', help='The destination filetype M4B, M4A or MP3', type=Filetypes)
+    parser.add_argument('-a', '--activation', help='Define the activation bits to use')
+    parser.add_argument('-s', '--single', help='Convert to a single file', action='store_true')
+    parser.add_argument('-l', '--lookup', help='Lookup the activation bits in the rainbow table', action='store_true')
+    parser.add_argument('-c', '--clear', help='Clear the activation bits cache', action='store_true')
 
-    activation_bits = args.activation if args.activation and not args.lookup else decrypt(args.file)
+    args = parser.parse_args()
 
-    if len(activation_bits) != 8:
-        print(Colors.FAIL + '::' + Colors.ENDC, 'Activation bits must be exactly 8 characters long')
-    else:
-        if activation_bits:
-            print(Colors.GREEN + '::' + Colors.ENDC, 'Activation bits found:', activation_bits)
+    try:
+        if args.clear:
+            saveCache({})
 
-            if not args.lookup:
-                ftype = args.type if args.type else Filetypes.M4B
-                convert(ftype, activation_bits, args.file, args.single)
+        activation_bits = args.activation if args.activation and not args.lookup else decrypt(args.file)
+
+        if len(activation_bits) != 8:
+            print(Colors.FAIL + '::' + Colors.ENDC, 'Activation bits must be exactly 8 characters long')
         else:
-            print(Colors.FAIL + '::' + Colors.ENDC, 'Failed to find activation bits')  
+            if activation_bits:
+                print(Colors.GREEN + '::' + Colors.ENDC, 'Activation bits found:', activation_bits)
 
-except FileNotFoundError as e:
-    print(Colors.FAIL + '::' + Colors.ENDC, 'No such file:', '\'' + args.file + '\'')
-except:
-    pass
+                if not args.lookup:
+                    ftype = args.type if args.type else Filetypes.M4B
+                    convert(ftype, activation_bits, args.file, args.single)
+            else:
+                print(Colors.FAIL + '::' + Colors.ENDC, 'Failed to find activation bits')  
+
+    except FileNotFoundError as e:
+        print(Colors.FAIL + '::' + Colors.ENDC, 'No such file:', '\'' + args.file + '\'')
+    except:
+        pass
